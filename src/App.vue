073b83d3f1c949f52815460c2777e331c77463f3
GauharChan <45651308+GauharChan@users.noreply.github.com>
2019-09-23 15:23:09 +0800

    完成电子书的渲染

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,17 @@
   </div>
 </template>
 
+<script>
+document.addEventListener('DOMContentLoaded',function(){
+  let html = document.querySelector('html')
+  let fontSize = window.innerWidth / 23
+  fontSize = fontSize > 50 ? '50px' : fontSize + 'px'
+  html.style.fontSize = fontSize
+})
+export default {
+  
+}
+</script>
+
 <style lang="scss">
 </style>
--- src/views/Ebook.vue
+++ src/views/Ebook.vue
@@ -1,13 +1,71 @@
 <template>
-    <div>
-        dfd
+  <div class="ebook">
+    <div id="book"></div>
+    <div class="mask">
+      <div class="left"></div>
+      <div class="center"></div>
+      <div class="right"></div>
     </div>
+  </div>
 </template>
+
 <script>
-    export default {
-        
+import Epub from "epubjs";
+
+// 静态资源必须放在public文件夹下
+// const DOWNLOAD_URL = '/AEN.epub'
+const DOWNLOAD_URL = "/unknownJS.epub";
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    showBook() {
+      // 创建Book对象
+      this.book = new Epub(DOWNLOAD_URL);
+      // 通过Book对象的renderTo方法生成rendtion
+      this.rendtion = this.book.renderTo("book", {
+        // book 是dom元素的id
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+      // 渲染电子书
+      this.rendtion.display();
     }
+  },
+  created() {
+    this.showBook();
+  }
+};
 </script>
-<style lang='scss' scoped>
-    
+<style lang="scss" scoped>
+@import "@/assets/styles/global.scss";
+// $fontSize: 16;
+// @function px2rem($px){
+//     @return ($px / $fontSize) + rem;
+// }
+.ebook {
+  position: relative;
+  .mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    // background: #bfa;
+    display: flex;
+    .left {
+      flex: 0 0 px2rem(100);
+    //   background: #000;
+    }
+    .center {
+      flex: 1;
+    //   background: red;
+    }
+    .right {
+      flex: 0 0 px2rem(100);
+    //   background: yellow;
+    }
+  }
+}
 </style>
