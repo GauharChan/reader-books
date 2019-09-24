b35480db3d3e67a4c1e0990622b87fe97a0a37e7
GauharChan <45651308+GauharChan@users.noreply.github.com>
2019-09-24 18:08:38 +0800

    完成顶部栏和底部栏

--- src/views/Ebook.vue
+++ src/views/Ebook.vue
@@ -1,10 +1,32 @@
 <template>
   <div class="ebook">
+    <!-- 头部菜单栏 -->
+    <div :class="{'title-top':true,'show':flag}">
+      <div class="title-left">
+        <span class="icon icon-xiaoyuhao"></span>
+      </div>
+      <div class="title-right">
+        <div class="right-icon-box">
+          <span class="icon icon-cart_icon"></span>
+          <span class="icon icon-wode"></span>
+          <span class="icon icon-gengduo"></span>
+        </div>
+      </div>
+    </div>
+    <!-- 电子书容器 -->
     <div id="book"></div>
+    <!-- 遮罩层 -->
     <div class="mask">
-      <div class="left"></div>
-      <div class="center"></div>
-      <div class="right"></div>
+      <div class="pre" @click="prevPage"></div>
+      <div class="center" @click="flag = !flag"></div>
+      <div class="next" @click="nextPage"></div>
+    </div>
+    <!-- 底部栏 -->
+    <div :class="{'bottom-box': true,'show':flag}">
+      <div class="icon-box"><span class="icon icon-mulu"></span></div>
+      <div class="icon-box"><span class="icon icon-huakuai"></span></div>
+      <div class="icon-box"><span class="icon icon-ai250"></span></div>
+      <div class="icon-box"><span class="icon icon-A"></span></div>
     </div>
   </div>
 </template>
@@ -17,7 +39,9 @@ import Epub from "epubjs";
 const DOWNLOAD_URL = "/unknownJS.epub";
 export default {
   data() {
-    return {};
+    return {
+      flag: false
+    };
   },
   methods: {
     showBook() {
@@ -31,6 +55,18 @@ export default {
       });
       // 渲染电子书
       this.rendtion.display();
+    },
+    prevPage() {
+      // Rendition.prev
+      if (this.rendtion) {
+        this.rendtion.prev();
+      }
+    },
+    nextPage() {
+      // Rendition.next
+      if (this.rendtion) {
+        this.rendtion.next();
+      }
     }
   },
   created() {
@@ -40,31 +76,76 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "@/assets/styles/global.scss";
-// $fontSize: 16;
-// @function px2rem($px){
-//     @return ($px / $fontSize) + rem;
-// }
+.show{
+  opacity: 1 !important;
+}
+
 .ebook {
   position: relative;
+  .title-top {
+    opacity: 0;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: px2rem(25);
+    box-shadow: 0px px2rem(-4) px2rem(10) black;
+    background-color: #fff;
+    .title-left {
+      @include center;
+    }
+    .right-icon-box {
+      width: px2rem(50);
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+  // 遮罩层
   .mask {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    // background: #bfa;
+    z-index: 100;
     display: flex;
-    .left {
+    .pre {
       flex: 0 0 px2rem(100);
-    //   background: #000;
     }
     .center {
       flex: 1;
-    //   background: red;
     }
-    .right {
+    .next {
       flex: 0 0 px2rem(100);
-    //   background: yellow;
+    }
+  }
+
+  // bottom
+  .bottom-box{
+    opacity: 0;
+    position: fixed;
+    bottom:0;
+    left: 0;
+    width: 100%;
+    height: px2rem(25);
+    display: flex;
+    z-index: 101;
+    box-shadow: 0 px2rem(4) px2rem(10) black;
+    background-color: #fff;
+    .icon-box{
+      flex: 1;
+      @include center;
+      .icon-huakuai{
+        font-size: px2rem(12);
+      }
+      .icon-A{
+        font-size: px2rem(7);
+      }
     }
   }
 }
